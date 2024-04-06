6f918b0cfa3a03d3c1114d057a1062f4f77a3a6e
vitor <pereiravitor1218@gmail.com>
2024-04-05 23:26:39 -0300

    feat(web): add button, dialgo, form, label, skeleton and toast components

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Header } from './components/header';
+import { Toaster } from './components/ui/toaster';
 import './globals.css';
 import ReactQueryProvider from './provider/react-query-provider';
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 				<ReactQueryProvider>
 					<Header />
 					{children}
+					<Toaster />
 				</ReactQueryProvider>
 			</body>
 		</html>
